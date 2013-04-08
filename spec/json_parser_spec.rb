e887c7cbc03314340c1a1dc98e68ebf21a0ceff8
Ian White <ian.w.white@gmail.com>
2013-04-08 21:01:16 +0100

    Piotr & Ian's attempt

--- spec/json_parser_spec.rb
+++ spec/json_parser_spec.rb
@@ -9,26 +9,25 @@ describe JavaScriptObjectNotationParser do
   let(:parser) { JavaScriptObjectNotationParser.new }
 
   def conversion_method_for_parse_as
-    :to_hash
+    :to_object
   end
 
   # Getting started
 
   describe "getting started" do
     example do
-      pending
       expect('').to parse_as(:nothing)
     end
   end
 
   describe "an empty object" do
     example do
-      pending
+      #pending
       expect('{}').to parse_as({ })
     end
 
     example do
-      pending
+      #pending
       # It's not important when you do this one,
       # it's just a sanity check
       expect('{   }').to parse_as({ })
@@ -39,34 +38,34 @@ describe JavaScriptObjectNotationParser do
 
   describe "string values" do
     example do
-      pending
+      #pending
       expect('{ "a" : "b" }').to parse_as({ "a" => "b" })
     end
 
     example do
-      pending
+      #pending
       expect('{ "AbC_1dE" : "aBc_2De" }').to parse_as({ "AbC_1dE" => "aBc_2De" })
     end
 
     example do
-      pending
+      #pending
       expect('{ "a" : "b" }').to parse_as({ "a" => "b" })
     end
   end
 
   describe "integer values" do
     example do
-      pending
+      #pending
       expect('{ "a" : 1 }').to parse_as({ "a" => 1 })
     end
 
     example do
-      pending
+      #pending
       expect('{ "a" : 123 }').to parse_as({ "a" => 123 })
     end
 
     example do
-      pending
+      #pending
       expect('{ "a" : -789 }').to parse_as({ "a" => -789 })
     end
   end
@@ -74,7 +73,7 @@ describe JavaScriptObjectNotationParser do
   # Now it gets harder
   describe "multiple member pairs" do
     example do
-      pending
+      #pending
       expect(
         '{ "a" : "b" , "c" : "d" , "e" : "f" }'
       ).to parse_as(
@@ -85,7 +84,7 @@ describe JavaScriptObjectNotationParser do
 
   describe "objects as values" do
     example do
-      pending
+      #pending
       expect(
         '{ "a" : { "x" : "y"} }'
       ).to parse_as(
@@ -97,32 +96,32 @@ describe JavaScriptObjectNotationParser do
   # Serious extra credit!
   describe "arrays" do
     example do
-      pending
+      #pending
       expect('{ "a" : [] }').to parse_as({ "a" => [ ] })
     end
 
     example do
-      pending
+      #pending
       expect('{ "a" : [1] }').to parse_as({ "a" => [1] })
     end
 
     example do
-      pending
+      #pending
       expect('{ "a" : ["b"] }').to parse_as({ "a" => ["b"] })
     end
 
     example do
-      pending
+      #pending
       expect('{ "a" : [1,2,3] }').to parse_as({ "a" => [1, 2, 3] })
     end
 
     example do
-      pending
+      #pending
       expect('{ "a" : [ 1 , 2 , 3] }').to parse_as({ "a" => [1, 2, 3] })
     end
 
     example do
-      pending
+      #pending
       expect(
         '{ "a" : [1, { "b" : 2 }, 3] }'
       ).to parse_as(
